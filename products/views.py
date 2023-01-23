547d4508a81d2c620e9a34f9afc7d58996618ef5
M. Samsioe <michael.samsioe@gmail.com>
2023-01-23 14:13:02 +0000

    Add view and template to products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Subscriptions, Pt_sessions
 
 # Create your views here.
+
+def subscriptions(request):
+    """ A view to show all gym subscriptions"""
+
+    subscriptions = Subscriptions.objects.all()
+
+    context = {
+        'subscriptios': subscriptions,
+    }
+
+    return render(request, 'products/subscriptions.html')
