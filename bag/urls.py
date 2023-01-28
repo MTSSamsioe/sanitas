a274ddd155b15e6bf9083729ee2e82134a9759e3
M. Samsioe <michael.samsioe@gmail.com>
2023-01-28 17:28:05 +0000

    Add update/removelinks + total to bag not tested

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_bag_item, name='add_bag_item'),
-    
+    path('update/<item_id>/', views.update_bag, name='update_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
 def view_bag(request):
     """ A view that renders the shopping bag """
@@ -7,7 +8,7 @@ def view_bag(request):
 
 
 def add_bag_item(request, item_id):
-    """ A View the adds products to shopping bag"""
+    """ A View that adds products to shopping bag"""
 
     quantity = int(request.POST.get('quantity'))
     bag_session = request.session.get('bag', {})
@@ -21,3 +22,27 @@ def add_bag_item(request, item_id):
     
 
     return  redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    """ A View the updates products in shopping bag"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    
+    bag = request.session.get('bag', {})
+
+    
+    
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+    
