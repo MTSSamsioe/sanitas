f801376d8079400d19599539d74f35686e8daa60
M. Samsioe <michael.samsioe@gmail.com>
2023-02-03 13:35:48 +0000

    Add changes to profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
 from .forms import ProfileForm
-
+from django.contrib import messages
 
 def profile(request):
     """
     Show users profiles
     """
     profile = get_object_or_404(Profile, user=request.user)
-    form = ProfileForm(instance=profile)
-    orders = profile.orders.all()
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile is updated')
+        else:
+            messages.error(request, 'Something went wrrong please try again')
+    else:
+        form = UserProfileForm(instance=profile)
+    
     context = {
         'form': form,
-        'orders': orders,
+        
     }
 
     return render(request, 'profiles/profile.html', context)
