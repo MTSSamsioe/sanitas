ae2db3c51d4b262fcf7a5105e01d63ce38d2af5c
M. Samsioe <michael.samsioe@gmail.com>
2023-02-10 13:46:44 +0000

    Delete google key and add new one to env.py and settings instead

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
+
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    GOOGLEKEY = settings.GOOGLEMAPS_KEY
+    
+    context = {
+        'GOOGLEKEY': GOOGLEKEY,
+    }
+    
+    return render(request, 'home/index.html', context)
--- sanitas_gym/settings.py
+++ sanitas_gym/settings.py
@@ -25,6 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# Google maps key:
+GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
