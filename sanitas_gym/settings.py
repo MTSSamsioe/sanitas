fe4f64bf5d6e6803edd11db63294b789e8e07dfe
M. Samsioe <michael.samsioe@gmail.com>
2023-02-14 08:19:29 +0000

    Removed secret key and set debug

--- sanitas_gym/settings.py
+++ sanitas_gym/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # Google maps key:
 GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sanitas-gym.heroku.app.com', 'localhost']
 
